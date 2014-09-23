c525224c4250c27aca0777b415c62847168de268
Daniel Langesten <daniel.langest@gmail.com>
2014-09-23 14:38:32 +0200

    created the project

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	//TODO
+	//load image file
+	//sort pixels
+	// preferably with modular algorithm
+	// also this step should be able to run in multiple go routines
+	//save output
+}
