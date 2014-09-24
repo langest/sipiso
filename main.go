c9b190fd02d18164cc3131b2ee14ee4f0998ac6f
Daniel Langesten <daniel.langest@gmail.com>
2014-09-24 09:09:58 +0200

    started working

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+
 func main() {
 	//TODO
 	//load image file
@@ -7,4 +9,15 @@ func main() {
 	// preferably with modular algorithm
 	// also this step should be able to run in multiple go routines
 	//save output
+}
+
+func openImage() error {
+	file, err := os.Open("path/to/image.img")
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+
+	//img := image.NewNRGBA
+	return nil
 }
